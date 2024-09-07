ad80379a0567196e1c430c19f755a188e99fc339
Maitreya <139490889+Maitruu@users.noreply.github.com>
2024-09-07 23:23:48 +0530

    Update Navbar.jsx

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = ({userInfo, onSearchNote, handleClearSearch}) => {
   return (
     <>
       <div className="bg-white flex items-center justify-between px-6 py-2 drop shadow">
-        <h2 className="text-xl font-bold text-black py-2">Notes</h2>
+        <h2 className="text-xl font-bold text-black py-2">NoteVerse</h2>
 
         <SearchBar
           value={searchQuery}
